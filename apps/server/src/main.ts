0c624b0424ba1afda64fffe6f4b4d064b1ad649c
Alan Wu <ac2wu@edu.uwaterloo.ca>
2022-01-16 01:07:56 -0500

    init

---
+++ apps/server/src/main.ts
@@ -0,0 +1,50 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { AppModule } from './app.module';
+import {
+  CorsConfig,
+  NestConfig,
+  SwaggerConfig,
+} from './configs/config.interface';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Validation
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Prisma Client Exception Filter for unhandled exceptions
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  const configService = app.get(ConfigService);
+  // @ts-ignore
+  const nestConfig = configService.get<NestConfig>('nest');
+  // @ts-ignore
+  const corsConfig = configService.get<CorsConfig>('cors');
+  // @ts-ignore
+  const swaggerConfig = configService.get<SwaggerConfig>('swagger');
+
+  // Swagger Api
+  if (swaggerConfig.enabled) {
+    const options = new DocumentBuilder()
+      .setTitle(swaggerConfig.title || 'Nestjs')
+      .setDescription(swaggerConfig.description || 'The nestjs API description')
+      .setVersion(swaggerConfig.version || '1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup(swaggerConfig.path || 'api', app, document);
+  }
+
+  // Cors
+  if (corsConfig.enabled) {
+    app.enableCors();
+  }
+
+  await app.listen(process.env.PORT || nestConfig.port || 3000);
+}
+bootstrap();
